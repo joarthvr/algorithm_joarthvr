a967fe0b82f8960d2b093ef773bc7eb2db8e0158
Arthur-Jo <iii1003@naver.com>
2024-04-12 01:59:34 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.45 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    var answer = '수';
+    console.log(answer[answer.length-1])
+    for(let i = 0; i < n -1; i++){
+    if(answer[answer.length -1] === '수'){
+        answer += '박';
+    }
+    else if(answer[answer.length -1] === '박'){
+        answer += '수';
+    }
+    }
+    return answer;
+}
+
+//배열의 인덱스가 홀수면 수 짝수면 박
