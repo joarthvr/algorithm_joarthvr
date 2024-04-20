d3516474e3bbd20682ab0b4c59f01923c33ec721
JoCheonSan <iii1003@naver.com>
2024-04-21 00:47:45 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -2,11 +2,8 @@ function solution(s) {
     const alphabet = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     for(let i = 0; i < s.length; i++){
         alphabet.forEach((e,i) => {
-        if(s.search(e) != -1){
+        if(s.search(e) !== -1){
             s = s.replace(e,i);
-        }
-        else{
-            return s;
         }
     });
     }
