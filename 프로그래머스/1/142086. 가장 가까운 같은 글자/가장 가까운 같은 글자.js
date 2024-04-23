b10590b6cb857a8396ac1454b651b8a75bd2755d
JoCheonSan <iii1003@naver.com>
2024-04-24 00:28:18 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 331.55 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,17 +1,12 @@
 function solution(s) {
     let ans = new Array(s.length).fill(-1);
     s = s.split("");
-    ans[0] = -1;
     s.forEach((e,idx)=>{
       for(let j = 0; j < idx; j++){
            if(s[j] == e){
                ans[idx] = idx - j;
            }
-          
        }
-        
-              
-          
         });
     
        return ans;
