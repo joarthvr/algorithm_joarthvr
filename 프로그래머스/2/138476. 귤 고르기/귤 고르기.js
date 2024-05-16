2639d6dce83209c901a471f24eff7b3f44ca890f
JoCheonSan <iii1003@naver.com>
2024-05-16 22:09:22 +0900

    [level 2] Title: 귤 고르기, Time: 49.72 ms, Memory: 41.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,21 @@
+function solution(k, tangerine) {
+    let size = [];
+    let q = 0;
+    tangerine = tangerine.sort((a,b)=>a-b);
+    for(let i = 0; i < tangerine.length; i++){
+        if(tangerine[i+1] !== tangerine[i]){
+        size.push(i+1 - q);
+        q=i+1;
+        }
+    }
+    console.log(size);
+    size = size.sort((a,b)=>b-a);
+    let i = 0;
+    let cnt = 0;
+    while(k > 0){
+        k = k-size[i]
+        cnt++;
+        i++;
+    }
+    return cnt;
+}
