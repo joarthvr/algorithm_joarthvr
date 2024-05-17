974ead64f20e7992323dddcdb6ff4e0f22e71d26
JoCheonSan <iii1003@naver.com>
2024-05-17 18:47:30 +0900

    [level 2] Title: 할인 행사, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,26 +1,28 @@
-function solution(want, number, discount) {
-    let cnt = 0;
-    let i = 0;
-    const period = 10;  // 확인할 기간(10일)
+function solution(want, number, discount){
+  let cnt = 0;
+  let i = 0;
+  const period = 10;
 
-    while (i <= discount.length - period) {
-        let arr = discount.slice(i, i + period);  // 10일 동안의 할인 품목
-        let valid = true;
-        
-        for (let j = 0; j < want.length; j++) {
-            let count = arr.reduce((cnt, element) => cnt + (want[j] === element), 0);
-            if (count !== number[j]) {
-                valid = false;
-                break;
-            }
-        }
+  while(i <= discount.length - period){
+    let arr = discount.slice(i, i+period);
+    let vaild = true;
 
-        if (valid) {
-            cnt++;
-        }
-
-        i++;
+    for(let j = 0; j < want.length; j++){
+        let count = arr.reduce((a, e) => {
+        if(want[j] === e)
+          a++;
+        return a;
+      }, 0);
+      if(count !== number[j]){
+        vaild = false;
+        break;
+      }
     }
-
-    return cnt;
+    if(vaild)
+      cnt++;
+      
+       i++;
+  }
+   
+  return cnt;
 }
