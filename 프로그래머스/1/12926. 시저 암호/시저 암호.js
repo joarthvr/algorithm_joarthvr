e35af7722e4c2186798f870a085a0a720a54254e
JoCheonSan <iii1003@naver.com>
2024-06-21 02:45:18 +0900

    [level 1] Title: 시저 암호, Time: 6.75 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,29 +1,55 @@
 function solution(s, n) {
-    var answer = '';
-    let char = '';
-    let k = 0;
+    // s = s.split("");
+    // console.log(s)
+    // s = s.join('')
+    // console.log(s)
+    // console.log(' '.charCodeAt());
+    // console.log(String.fromCharCode(32))
+    let j;
+    let c;
+    let k;
+    let a;
+    let y = [];
     for(let i = 0; i < s.length; i++){
-        char = s[i];
-        k = char.charCodeAt() + n;
-        if(char.charCodeAt() > 64 && char.charCodeAt() < 91){
-            if(k>90){
-                answer += String.fromCharCode(k-26);
-            }
-            else{
-                answer += String.fromCharCode(k);
-            }
+        j = s[i].charCodeAt();
+        if(j >=65 && j<= 90){
+        if(j+n > 90){
+            k = j+n -91;
+            c = 65 + k;
+            a = String.fromCharCode(c);
+            y.push(a);
+            continue;
         }
-        else if(char.charCodeAt() > 96 && char.charCodeAt() < 123){
-           
-           if(k>122){
-                answer += String.fromCharCode(k-26);
-            }
-            else{
-                answer += String.fromCharCode(k);
-            } 
-        }else{
-            answer += s[i];
+        else{
+            c = j + n;
+           a = String.fromCharCode(c);
+            y.push(a);
+            continue;
         }
+        }
+        if(j >=97 && j<= 122){
+        if(j + n > 122){
+            k = j+n - 123;
+            c = 97 + k;
+            a = String.fromCharCode(c);
+            y.push(a);
+            continue;
+        }
+        else{
+            c = j + n;
+            a = String.fromCharCode(c);
+            y.push(a);
+            continue;
+        }}
+        if(j === 32){
+            y.push(" ");
+            continue;
+        }
+        
     }
-    return answer;
+    y = y.join("")
+    return y;
 }
+//공백은 밀어도 공백
+//소문자, 대문자, 공백으로만 이뤄짐
+
