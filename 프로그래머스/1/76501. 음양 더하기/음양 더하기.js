33a521e2b088a5f27f9bce47a692f0d161be3ec6
JoCheonSan <iii1003@naver.com>
2024-06-21 03:09:01 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,9 +1,6 @@
 function solution(absolutes, signs) {
-    return absolutes.reduce((sum, e, i) => {  // 여기서 i는 reduce에서 제공하는 인덱스
-        if (signs[i]) {
-            return sum + e;  // signs[i]가 true면 e를 더함
-        } else {
-            return sum - e;  // signs[i]가 false면 e를 뺌
-        }
-    }, 0);  // 초기값 0
+    const result = absolutes.reduce((acc,element,index) =>{
+        return signs[index] === true ? acc+=element : acc -= element;
+    },0);
+    return result;
 }
