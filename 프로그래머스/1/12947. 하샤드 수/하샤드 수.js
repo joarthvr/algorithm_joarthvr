95daf028dd50f1626600fb8ce5982596aca49b30
JoCheonSan <iii1003@naver.com>
2024-06-21 02:56:17 +0900

    [level 1] Title: 하샤드 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,13 +1,10 @@
 function solution(x) {
-    let x1 = x.toString();
-    let size = x1.length;
+    x = x.toString();
+    x = x.split("");
+    x = x.join("")
     let sum = 0;
-    for(let i = 0; i < size; i++){
-            sum += x % 10;
-            x = Math.floor(x / 10);
-            console.log(x);
+    for(let i = 0; i < x.length; i++){
+        sum += Math.floor(x[i]);
     }
-    console.log(sum);
-    return Math.floor(x1) % sum === 0 ? true : false;
-    
+    return Math.floor(x) % sum === 0 ? true :  false
 }
