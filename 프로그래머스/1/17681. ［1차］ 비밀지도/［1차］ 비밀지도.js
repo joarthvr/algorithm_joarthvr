626e4ce71955c82a27582d8f1435b85dc33f4ca7
JoCheonSan <iii1003@naver.com>
2024-06-23 01:20:31 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,32 +1,19 @@
 function solution(n, arr1, arr2) {
-    let ans = [];
-    arr1 = arr1.map(e => {
-        let binary = e.toString(2);
-        while (binary.length < n) {
-            binary = '0' + binary;
-        }
-        return binary;
-    });
-    console.log(arr1);
-
-    arr2 = arr2.map(e => {
-        let binary = e.toString(2);
-        while (binary.length < n) {
-            binary = '0' + binary;
-        }
-        return binary;
-    });
-    console.log(arr2);
-    
-    for (let i = 0; i < arr1.length; i++) {
-        ans[i] = [];  // 각 행에 대한 배열 초기화
-        for (let j = 0; j < arr1[i].length; j++) {
-            if (arr1[i][j] === '0' && arr2[i][j] === '0') {
-                ans[i] += " ";
-            } else {
-                ans[i] += "#";
+    arr1 = arr1.map(e => e.toString(2).padStart(n, '0'));
+    arr2 = arr2.map(e => e.toString(2).padStart(n, '0'));
+    let result = [];
+    let tmp = ""
+    for(let i = 0; i < n; i++){
+        tmp = ""
+        for(let j = 0; j < n; j++){
+            if(arr1[i][j] === '0' && arr2[i][j] === '0'){
+                tmp +=" ";
+            }
+            else{
+                tmp +="#";
             }
         }
+        result.push(tmp);
     }
-    return ans;
+    return result
 }
