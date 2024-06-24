1692ad2c9993b01bd433f3e4d0cfe00270956b19
JoCheonSan <iii1003@naver.com>
2024-06-24 23:06:03 +0900

    [level 1] Title: 기사단원의 무기, Time: 2.47 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,20 +1,31 @@
 function solution(number, limit, power) {
-  let answer = 0;
-
-  for (let i = 1; i <= number; i++) {
-    let divisor = 0;
-    for (let j = 1; j <= Math.sqrt(i); j++) {
-      if (i % j === 0) {
-        if (i / j === j) divisor += 1;
-        else divisor += 2;
-      }
-      if (divisor > limit) {
-        divisor = power;
-        break;
-      }
+    // 각 숫자의 약수 개수를 저장할 배열
+    let cnt = new Array(number).fill(0);
+    
+    // 약수 개수 계산
+    for (let i = 0; i < number; i++) {
+        let divisorsCount = 0;
+        for (let k = 1; k <= Math.sqrt(i + 1); k++) {
+            if ((i + 1) % k === 0) {
+                if (k === (i + 1) / k) {
+                    divisorsCount++; // 제곱근일 때
+                } else {
+                    divisorsCount += 2; // 서로 다른 약수일 때
+                }
+            }
+        }
+        cnt[i] = divisorsCount;
     }
-    answer += divisor;
-  }
-  
-  return answer;
+
+    cnt = cnt.map(element => {
+        if (element > limit) {
+            return power;
+        }
+        return element;
+    });
+
+    // 배열의 합 계산
+    let result = cnt.reduce((a, e) => a + e, 0);
+    
+    return result;
 }
