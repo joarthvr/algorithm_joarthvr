56bdd0e1f0590158acfe731fae2d2ae0a4dd8f1d
JoCheonSan <iii1003@naver.com>
2024-06-24 19:27:13 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,15 +1,19 @@
 function solution(t, p) {
-
-    let cnt = 0;
-    t = t.toString();
-    let tmp = 0;
-   for(let i = 0; i < t.length; i++){
-      tmp = t.slice(i, i+p.length);
-       if(tmp.length === p.length && Math.floor(tmp) <= p) {
-            console.log(tmp);
-           cnt ++;
-       }
-   }
-    return cnt;
+    let split = [];
+    for(let i = 0; i < t.length; i++){
+        if(i + p.length > t.length)
+            break;
+        split.push(t.slice(i,i+p.length));
+    }
+    
+   let result = split.reduce((acc, element) => {
+    if (Math.floor(element) <= Math.floor(p)) {
+        acc++;
+    }
+    return acc;
+}, 0);
+    return result;
 }
+//p의 크기로 t를 나눔
+//p의 크기보다 작거나 같은 수를 셈
 
