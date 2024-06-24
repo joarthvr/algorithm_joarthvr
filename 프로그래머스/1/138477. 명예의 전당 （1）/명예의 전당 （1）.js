8d6654128222fbb84af9c2397844e79c461d0dfc
JoCheonSan <iii1003@naver.com>
2024-06-24 21:44:06 +0900

    [level 1] Title: 명예의 전당 (1), Time: 3.57 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,10 +1,26 @@
 function solution(k, score) {
-    var answer = [];
-    var nums = [];
-    score.forEach(num => {
-        nums.push(num);
-        nums.sort((a,b) => b-a).splice(k);
-        answer.push(Math.min.apply(null, nums));
-    });
-    return answer;
-}
+    let honor = [];
+    let result = [];
+    for(let i = 0; i < score.length; i++){
+        if(honor.length < k ){
+            honor.push(score[i]);
+            result.push(Math.min(...honor));
+            honor.sort((a,b)=>b-a);
+            console.log(honor)
+            continue;
+        }
+        if(Math.min(...honor) <= score[i]){
+            honor.pop();
+            honor.push(score[i]);
+            result.push(Math.min(...honor))
+            honor.sort((a,b)=>b-a);
+            console.log(honor)
+            continue;
+        }
+        else{
+            result.push(Math.min(...honor))
+        }
+        
+    }
+    return result;
+   }
