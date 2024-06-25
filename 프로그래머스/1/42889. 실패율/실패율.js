a320a65863d2988fafdea28dc843305867cd7a09
JoCheonSan <iii1003@naver.com>
2024-06-25 15:17:32 +0900

    [level 1] Title: 실패율, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,11 +1,31 @@
 function solution(N, stages) {
-    let result = [];
-    for(let i=1; i<=N; i++){
-        let reach = stages.filter((x) => x >= i).length;
-        let curr = stages.filter((x) => x === i).length;
-        result.push([i, curr/reach]);
-       
+    let myObject = {};
+    
+    for (let i = 1; i <= N; i++) {
+        let moreThan = 0;
+        let stopped = 0;
+        
+        for (let j = 0; j < stages.length; j++) {
+            if (i <= stages[j]) {
+                moreThan++;
+            }
+            if (i === stages[j]) {
+                stopped++;
+            }
+        }
+        
+        // 실패율 계산
+        let value = moreThan === 0 ? 0 : stopped / moreThan;
+        myObject[i] = value;
     }
-    result.sort((a,b) => b[1] - a[1]);
-    return result.map((x) => x[0]);
+    
+    // 객체를 배열로 변환하여 실패율 기준으로 정렬
+    let result = Object.keys(myObject).sort((a, b) => {
+        if (myObject[b] === myObject[a]) {
+            return a - b; // 실패율이 같으면 스테이지 번호가 작은 순으로 정렬
+        }
+        return myObject[b] - myObject[a];
+    }).map(Number); // 문자열을 숫자로 변환
+    
+    return result;
 }
