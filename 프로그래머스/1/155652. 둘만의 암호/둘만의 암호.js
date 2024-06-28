5f11542a6c87057843427272fcfc70f8bbbfbbea
JoCheonSan <iii1003@naver.com>
2024-06-29 06:25:37 +0900

    [level 1] Title: 둘만의 암호, Time: 0.15 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,19 @@
+function solution(s, skip, index) {
+    let spelling = ['a','b','c','d','e','f','g','h','i',
+                    'j','k','l','m','n','o','p','q','r',
+                    's','t','u','v','w','x','y','z'];
+    for(let i =0; i < skip.length; i++){
+        if(spelling.includes(skip[i])){
+            spelling.splice(spelling.indexOf(skip[i]),1)
+        }
+    }
+    let result = ""
+    for(let i = 0; i < s.length; i++){
+        let skippedIndex = spelling.indexOf(s[i])+index;
+        if(skippedIndex >= spelling.length)
+            skippedIndex = skippedIndex % spelling.length;
+        //console.log(skippedIndex)
+        result+=(spelling[skippedIndex]);
+    }
+    return result;
+}
