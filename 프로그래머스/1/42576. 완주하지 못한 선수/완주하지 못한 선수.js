5a0791083da64929e6e986f63fb224cf10800bca
JoCheonSan <iii1003@naver.com>
2024-07-06 22:20:06 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 90.45 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,26 +1,12 @@
 function solution(participant, completion) {
-    let participantCount = {};
-
-    // 참가자 배열의 각 요소를 카운트하여 해시맵에 저장합니다.
-    participant.forEach(e => {
-        if (participantCount[e]) {
-            participantCount[e]++;
-        } else {
-            participantCount[e] = 1;
-        }
-    });
-
-    // 완주자 배열의 각 요소에 대해 해시맵에서 해당 카운트를 감소시킵니다.
-    completion.forEach(e => {
-        if (participantCount[e]) {
-            participantCount[e]--;
-        }
-    });
-
-    // 카운트가 0이 아닌 요소를 찾아서 반환합니다.
-    for (let key in participantCount) {
-        if (participantCount[key] > 0) {
-            return key;
+    var answer = '';
+    participant.sort();
+    completion.sort();
+    for(var i = 0 ; i < participant.length; i++){
+        if(participant[i] !== completion[i]){
+            answer = participant[i];
+            break;
         }
     }
+    return answer;
 }
