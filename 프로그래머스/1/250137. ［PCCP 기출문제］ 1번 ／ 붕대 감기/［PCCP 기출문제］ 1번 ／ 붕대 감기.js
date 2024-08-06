b6be5a29714831175269551830eba1bb72b043e5
Jo Cheonsan <iii1003@naver.com>
2024-08-06 23:48:51 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.57 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -1,45 +1,49 @@
 function solution(bandage, health, attacks) {
-   const TOTALTIME = attacks[attacks.length-1][0];
-   const attacksObj = attacks.reduce((acc, [key, value]) => {
-    acc[key] = value;
+  const TOTAL_TIME = attacks[attacks.length - 1][0];
+  const attackTimes = attacks.reduce((acc, [time, damage]) => {
+    acc[time] = damage;
     return acc;
-    }, {});
-    const howLongMaintain = bandage[0];
-    const recoveryAmount = bandage[1];
-    const bonusRecovery = bandage[2];
-    let count = 0;
-    const MAXHEALTH = health
-   for(let i = 0; i <= TOTALTIME; i++){
-       count++;
-       if(attacksObj[i]){
-           count = 0;
-           health -= attacksObj[i];
-           if(health <= 0 )
-           return -1;
-           console.log("어택 " + health)
-           continue;
-       }
-       if(count === howLongMaintain){
-           count = 0;
-           health += recoveryAmount + bonusRecovery;
-           if(health >= MAXHEALTH)
-           health = MAXHEALTH
-           if(health <= 0 )
-           return -1;
-           console.log("보너스 " + health)
-           continue;
-       }
-        if(health <= 0 )
-           return -1;
-       health += recoveryAmount
-       if(health >= MAXHEALTH)
-           health = MAXHEALTH
-       console.log(health)
-   }
-       return health;
+  }, {});
+  
+  const [maintainTime, recoveryAmount, bonusRecovery] = bandage;
+  let healthRecoveryCounter = 0;
+  const MAX_HEALTH = health;
+  
+  // 함수: 공격을 처리하고, 체력을 감소시키는 함수
+  const applyAttack = (currentHealth, damage) => {
+    currentHealth -= damage;
+    return currentHealth;
+  };
+  
+  // 함수: 체력을 회복시키는 함수
+  const recoverHealth = (currentHealth, amount) => {
+    currentHealth += amount;
+    return Math.min(currentHealth, MAX_HEALTH); // 최대 체력을 넘지 않도록 함
+  };
+  
+  for (let time = 0; time <= TOTAL_TIME; time++) {
+    healthRecoveryCounter++;
+    
+    // 공격이 있는 경우
+    if (attackTimes[time]) {
+      healthRecoveryCounter = 0;
+      health = applyAttack(health, attackTimes[time]);
+      if (health <= 0) return -1;
+      continue;
+    }
+    
+    // 체력 유지 시간이 경과한 경우
+    if (healthRecoveryCounter === maintainTime) {
+      healthRecoveryCounter = 0;
+      health = recoverHealth(health, recoveryAmount + bonusRecovery);
+      if (health <= 0) return -1;
+      continue;
+    }
+    
+    // 0 이하일 경우
+    health = recoverHealth(health, recoveryAmount);
+    if (health <= 0) return -1;
+  }
+  
+  return health;
 }
-
-
-//[시전시간,1초당 회목량, 추가회복략]
-//최대체력
-//몬드터의 [공격시간, 피해량]
