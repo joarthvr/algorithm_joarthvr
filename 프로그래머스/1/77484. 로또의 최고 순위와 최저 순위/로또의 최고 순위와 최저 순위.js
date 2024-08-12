22dd2695c687434ec7b731060b6983bee712a2f1
Jo Cheonsan <iii1003@naver.com>
2024-08-12 11:10:51 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -2,7 +2,7 @@ function solution(lottos, win_nums) {
     let score = [6,6,5,4,3,2,1];
     let min = 0;
     let max = 0;
-    let zeroCnt = lottos.reduce((count, number, index) => {
+    let zeroCnt = lottos.reduce((count, number) => {
     if (number === 0) {
         count++;
     }
@@ -10,7 +10,6 @@ function solution(lottos, win_nums) {
 }, 0);
 // 0을 모두 제거한 새로운 배열을 생성
 lottos = lottos.filter(number => number !== 0);
-    console.log(zeroCnt);
     for(let i = 0; i < win_nums.length; i++){
         for(let j = 0; j < lottos.length; j++){
             if(win_nums.includes(lottos[j])){
@@ -20,7 +19,6 @@ lottos = lottos.filter(number => number !== 0);
             }
         }
     }
-    // console.log(min)
     min + 2 > 6 ? max = 6 : max = min+zeroCnt;
     return [score[max], score[min]];
 }
