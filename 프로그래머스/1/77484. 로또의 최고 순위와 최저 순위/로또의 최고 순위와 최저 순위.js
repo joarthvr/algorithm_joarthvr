d1f246c7675e585060738df013cb69435342784b
Jo Cheonsan <iii1003@naver.com>
2024-08-15 17:41:01 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,24 +1,8 @@
 function solution(lottos, win_nums) {
-    let score = [6,6,5,4,3,2,1];
-    let min = 0;
-    let max = 0;
-    let zeroCnt = lottos.reduce((count, number) => {
-    if (number === 0) {
-        count++;
-    }
-    return count;
-}, 0);
-// 0을 모두 제거한 새로운 배열을 생성
-lottos = lottos.filter(number => number !== 0);
-    for(let i = 0; i < win_nums.length; i++){
-        for(let j = 0; j < lottos.length; j++){
-            if(win_nums.includes(lottos[j])){
-                lottos.splice(lottos.indexOf(lottos[j]),1)
-                min++;
-                continue;
-            }
-        }
-    }
-    min + 2 > 6 ? max = 6 : max = min+zeroCnt;
-    return [score[max], score[min]];
+    const score = [6, 6, 5, 4, 3, 2, 1];  // 맞춘 개수에 따른 순위 배열
+    const min = lottos.filter(num => win_nums.includes(num)).length;  // 최소 맞춘 개수
+    const zeroCnt = lottos.filter(num => num === 0).length;  // 0의 개수
+    const max = min + zeroCnt;  // 최대로 맞출 수 있는 개수
+
+    return [score[max], score[min]];  // 최고 순위와 최저 순위 반환
 }
