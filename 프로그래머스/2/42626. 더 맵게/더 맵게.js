d5a19feaa36ca1df83b1902fd691349a615d2448
Jo Cheonsan <iii1003@naver.com>
2024-09-05 11:16:55 +0900

    [level 2] Title: 더 맵게, Time: 483.64 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -0,0 +1,74 @@
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    push(value) {
+        this.heap.push(value);
+        this.bubbleUp();
+    }
+
+    pop() {
+        if (this.isEmpty()) return null;
+        if (this.heap.length === 1) return this.heap.pop();
+
+        const min = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.bubbleDown();
+        return min;
+    }
+
+    isEmpty() {
+        return this.heap.length === 0;
+    }
+
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            const parentIndex = Math.floor((index - 1) / 2);
+            if (this.heap[parentIndex] <= this.heap[index]) break;
+            [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
+            index = parentIndex;
+        }
+    }
+
+    bubbleDown() {
+        let index = 0;
+        while (true) {
+            const leftChild = 2 * index + 1;
+            const rightChild = 2 * index + 2;
+            let smallest = index;
+
+            if (leftChild < this.heap.length && this.heap[leftChild] < this.heap[smallest]) {
+                smallest = leftChild;
+            }
+
+            if (rightChild < this.heap.length && this.heap[rightChild] < this.heap[smallest]) {
+                smallest = rightChild;
+            }
+
+            if (smallest === index) break;
+
+            [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
+            index = smallest;
+        }
+    }
+}
+
+function solution(scoville, K) {
+    const minHeap = new MinHeap();
+    for (const scov of scoville) {
+        minHeap.push(scov);
+    }
+
+    let mixCount = 0;
+    while (minHeap.heap[0] < K && minHeap.heap.length > 1) {
+        const first = minHeap.pop();
+        const second = minHeap.pop();
+        const newScov = first + (second * 2);
+        minHeap.push(newScov);
+        mixCount++;
+    }
+
+    return minHeap.heap[0] >= K ? mixCount : -1;
+}
