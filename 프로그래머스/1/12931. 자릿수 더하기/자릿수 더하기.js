016794a6bd2902520bff63d328548bd2abb2b9b8
Jo Cheonsan <iii1003@naver.com>
2025-02-17 09:58:22 +0900

    [level 1] Title: 자릿수 더하기, Time: 5.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,10 +1,14 @@
 function solution(n){
-    n = n + "";
-    n = n.split("");
+    //1. 각 자리수의 자연수를 구한다
+    //2. 구한 자연수를 모두 더한다 
+    
     let sum = 0;
-    n.map((e) =>{
-        e = parseInt(e);
-        sum += e;
-    })
+    n = n + "";
+    console.log(n.split(""));
+    
+    for(let i = 0; i < n.length; i++){
+        sum += Math.floor(n[i]);
+    }
+    
     return sum;
 }
