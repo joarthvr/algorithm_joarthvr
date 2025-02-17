c8b2624d7d6c18bac0c15651f38d3c1761163fc8
Jo Cheonsan <iii1003@naver.com>
2025-02-17 09:45:26 +0900

    [level 1] Title: 약수의 합, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,15 +1,13 @@
 function solution(n) {
-    let ans =[];
-    let j = 0;
+    //1. n의 약수를 구한다
+    //2. n약수를 모두 더한다
     let sum = 0;
+    let tmp = 0;
     for(let i = 1; i <= n; i++){
         if(n % i === 0){
-            ans[j] = i;
-            j++;
+            tmp = n/i;
+            sum += tmp + i;
         }
     }
-    ans.forEach((e) =>{ sum += e;})
-       
-    
-    return sum;
+    return sum/2;
 }
