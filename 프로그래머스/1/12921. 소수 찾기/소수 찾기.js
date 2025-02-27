baf02f611109ff1bc341fd4d016e8175268683f4
Jo Cheonsan <iii1003@naver.com>
2025-02-27 16:37:59 +0900

    [level 1] Title: 소수 찾기, Time: 47.59 ms, Memory: 43.1 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -1,19 +1,24 @@
 function solution(n) {
-  // 2부터 n까지의 수로 구성된 Set
-  const s = new Set();
-  for (let i = 2; i <= n; i++) {
-    s.add(i);
-  }
-
-  // 2부터 n의 제곱근보다 작은 최대 정수까지 
-  for (let j = 2; j < Math.sqrt(n); j++) {
-    // Set에 해당 수가 포함되면
-    if (s.has(j)) {
-      // 그 수를 제외하고 그 수의 배수는 모두 삭제
-      for (let k = j * 2; k <= n; k += j) {
-        s.delete(k);
-      }
+    let result = 0;
+    const arr = new Array(n+1).fill(true); 
+    const end = Math.sqrt(n);
+    
+    for(let i = 2; i <= end; i++){
+        if(arr[i] === false){
+            continue;
+        }
+        
+        for(let j = i * 2; j <= n; j+=i){
+            if(arr[j]){
+                arr[j] = false;
+            }
+        }
+        
     }
-  }
-  return s.size;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i]){
+            result++;
+        }
+    }
+    return result-2;
 }
