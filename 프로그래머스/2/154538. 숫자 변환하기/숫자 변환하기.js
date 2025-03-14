7e2aaf9d8f5b7178ab5ebfb5f8fc38316fdf1445
Jo Cheonsan <iii1003@naver.com>
2025-03-14 15:24:10 +0900

    [level 2] Title: 숫자 변환하기, Time: 104.31 ms, Memory: 58.8 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,32 +1,25 @@
 function solution(x, y, n) {
-    if (x === y) return 0;
-    if (x > y) return -1;
-    
-    // 최적화: 배열 대신 객체 사용
-    const dist = {};
-    dist[x] = 0;
-    
-    // 최적화: 배열 대신 더 효율적인 큐 구현
-    const queue = [x];
-    let front = 0;
-    
-    while (front < queue.length) {
-        const current = queue[front++];
-        const nextCount = dist[current] + 1;
-        
-        // 세 가지 가능한 다음 상태
-        const nextStates = [current + n, current * 2, current * 3];
-        
-        for (const next of nextStates) {
-            if (next === y) return nextCount;
-            
-            // 범위를 벗어나거나 이미 방문한 상태 스킵
-            if (next > y || dist[next] !== undefined) continue;
-            
-            dist[next] = nextCount;
-            queue.push(next);
+  let answer = 0;
+  if (x === y) {
+    return answer;
+  }
+  let queue = [x];
+  const set = new Set(queue);
+  while (queue.length !== 0) {
+    const newQueue = [];
+    answer += 1;
+    for (const num of queue) {
+      for (const newNum of [num + n, 2 * num, 3 * num]) {
+        if (newNum === y) {
+          return answer;
         }
+        if (newNum < y && !set.has(newNum)) {
+          set.add(newNum);
+          newQueue.push(newNum);
+        }
+      }
     }
-    
-    return -1;
+    queue = newQueue;
+  }
+  return -1;
 }
