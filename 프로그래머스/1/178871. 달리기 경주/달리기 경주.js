5f478a451791ec7a0f64f37428c03819f2416940
Jo Cheonsan <iii1003@naver.com>
2025-04-11 16:21:34 +0900

    [level 1] Title: 달리기 경주, Time: 347.80 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,29 +1,22 @@
 function solution(players, callings) {
-    // 선수 이름 -> 위치 매핑
-    const playerToIndex = {};
+    const playerToIdx = {};
     
-    // 초기 위치 저장
-    for (let i = 0; i < players.length; i++) {
-        playerToIndex[players[i]] = i;
+    for(let i = 0; i < players.length; i++){
+        playerToIdx[players[i]] = i;
     }
     
-    for (const name of callings) {
-        // 호출된 선수의 현재 위치
-        const currentIdx = playerToIndex[name];
-        
-        if (currentIdx > 0) {
-            // 앞 선수의 이름
-            const frontPlayer = players[currentIdx - 1];
-            
-            // 위치 교환
-            players[currentIdx - 1] = name;
-            players[currentIdx] = frontPlayer;
-            
-            // 위치 정보 업데이트
-            playerToIndex[name] = currentIdx - 1;
-            playerToIndex[frontPlayer] = currentIdx;
-        }
-    }
-    
+   for(const name of callings){
+       let frontPlayer = players[playerToIdx[name] -1];
+       
+       //플레이어 배열 위치 교환
+       players[playerToIdx[name]] = frontPlayer;
+       players[playerToIdx[name] -1] = name;
+       
+       //플레이어 객체 인덱스 업데이트
+       playerToIdx[name]--;
+       playerToIdx[frontPlayer]++;
+       
+       
+   }
     return players;
 }
