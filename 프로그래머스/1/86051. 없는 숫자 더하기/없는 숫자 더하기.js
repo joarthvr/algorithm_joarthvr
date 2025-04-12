4f6bbc20f47f879cdbc720ab0e659f1935cfd192
Jo Cheonsan <iii1003@naver.com>
2025-04-12 20:17:05 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,7 +1,3 @@
 function solution(numbers) {
-    const total = 45;
-    const sum = numbers.reduce((s,e,i) =>{
-        return s += e;
-    }, 0)
-    return total - sum;
+   return 45 - numbers.reduce((a,e) => a+e,0);
 }
