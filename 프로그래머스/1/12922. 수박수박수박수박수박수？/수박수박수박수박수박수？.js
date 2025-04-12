58954a4ec0d5638972286f928e8a724590bf0ea2
Jo Cheonsan <iii1003@naver.com>
2025-04-12 21:13:06 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.81 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,14 +1,9 @@
 function solution(n) {
-    var answer = '수';
-    for(let i = 0; i < n -1; i++){
-    if(answer[answer.length -1] === '수'){
-        answer += '박';
-    }
-    else if(answer[answer.length -1] === '박'){
-        answer += '수';
-    }
-    }
-    return answer;
+    let ans = ""
+    let i = 1;
+    while(i <= n){
+        ans += i % 2 === 0 ? "박" : "수";
+        i++;
+   }
+    return ans;
 }
-
-//배열의 인덱스가 홀수면 수 짝수면 박
