0c78b8972db774b24c869e36715a6feb85bb9de0
Jo Cheonsan <iii1003@naver.com>
2025-04-12 20:09:53 +0900

    [level 1] Title: 하샤드 수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,8 +1,4 @@
 function solution(x) {
-    x = x.toString();
-    let sum = 0;
-    for(let i = 0; i < x.length; i++){
-        sum += Math.floor(x[i]);
-    }
-    return Math.floor(x) % sum === 0 ? true :  false
+    return x % String(x).split("").reduce((a,e) => a + Number(e), 0) === 0 ? true : false;
+    
 }
