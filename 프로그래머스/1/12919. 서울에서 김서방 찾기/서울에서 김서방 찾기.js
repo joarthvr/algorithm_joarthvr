3566f3fb62733edc3023890afa5b67b92a28304e
Jo Cheonsan <iii1003@naver.com>
2025-04-12 20:26:42 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,4 +1,4 @@
 function solution(seoul) {
-    const ans = seoul.indexOf("Kim");
-    return "김서방은 " + ans +"에 있다"   
+    const idx = String(seoul.findIndex((a) =>a === "Kim"));
+    return "김서방은 " + idx+"에 있다";
 }
