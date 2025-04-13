aeaa0d7d4310b63ce1f7c91ac7895bfc4877b6bc
Jo Cheonsan <iii1003@naver.com>
2025-04-13 16:59:46 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,10 +1,11 @@
 function solution(n, m) {
-    let co1 = 1;
-    for(let i = 2; i <= n; i++){
-        if(m % i === 0 && n % i === 0)
-            co1 = i;
+    const min = Math.min(n,m);
+    let divisor = 0;
+    for(let i = 1; i <= min; i++){
+        if(n % i === 0 && m % i === 0) divisor = i
     }
-    let comMul = (n * m) / co1;
-    return [co1, comMul];
+    const multiple = (n * m) / divisor;
+    
+    return [divisor, multiple];
 }
 // 두 자연수의 곱은 두수의 최대공약수 x 최소공배수
