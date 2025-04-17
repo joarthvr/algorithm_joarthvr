f75d8130fe09128067b8e4fa56185db5092174bb
Jo Cheonsan <iii1003@naver.com>
2025-04-17 22:22:20 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,12 +1,20 @@
 function solution(n) {
-    const binaryN = n.toString(2);
+    // 2진수에서 1의 개수 계산
+    const countOnes = (num) => {
+        let count = 0;
+        while (num > 0) {
+            if (num & 1) count++; // 최하위 비트 확인
+            num >>>= 1;           // 부호 없는 오른쪽 시프트
+        }
+        return count;
+    };
     
-    const standard = binaryN.split("").reduce((acc,curr) => acc += curr ==='1',0);
-    while(1){
-        let i = ++n;
-        let binaryI = n.toString(2);
-        let cntOne = binaryI.split("").reduce((acc,curr) => acc += curr ==='1',0);
-        if(standard === cntOne) return i;
-        
+    const nOnes = countOnes(n);
+    let next = n + 1;
+    
+    while (countOnes(next) !== nOnes) {
+        next++;
     }
+    
+    return next;
 }
