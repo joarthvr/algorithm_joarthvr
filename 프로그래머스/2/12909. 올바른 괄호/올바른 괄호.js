ac6da694056cbae1a1ac8ec9dd937ace21df2aed
Jo Cheonsan <iii1003@naver.com>
2025-04-17 20:09:53 +0900

    [level 2] Title: 올바른 괄호, Time: 34.48 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,36 +1,22 @@
 function solution(s) {
-    class Stack {
-        constructor() {
-            this.storage = {};
-            this.size = 0;
+    const stack = [];
+    s = s.split("");
+    let ans = true;
+    
+    for(let i = 0; i < s.length; i++){
+        const top = stack[stack.length-1];
+        if(top === '(' && s[i] === ')'){
+            stack.pop();
         }
-        push(element) {
-            this.size++;
-            this.storage[this.size] = element;
-        }
-        pop() {
-            let removed = this.storage[this.size];
-            delete this.storage[this.size];
-            this.size--;
-            return removed;
-        }
-        top() {
-            return this.storage[this.size];
+        else{
+            stack.push(s[i]);
         }
     }
-
-    s = s.split('');
-    const stack = new Stack();
-    for (let i = 0; i < s.length; i++) {
-        if (stack.top() === "(" && s[i] === ")") {
-            stack.pop(); // 올바른 괄호 쌍을 찾으면 스택에서 pop
-        } else {
-            stack.push(s[i]); // 그 외의 경우에는 스택에 push
-        }
-    }
-    return stack.size > 0 ? false : true
+    console.log(stack);
+    if(stack.length > 0) ans = false;
+    return ans;
 }
-// ?????
+
 
     
     
