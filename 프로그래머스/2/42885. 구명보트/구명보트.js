372a988c9d1374265bcb9f64b6b332ca850b2789
Jo Cheonsan <iii1003@naver.com>
2025-04-18 13:58:14 +0900

    [level 2] Title: 구명보트, Time: 69.21 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,15 +1,25 @@
 function solution(people, limit) {
-    let count = 0;
-    people = people.sort((a,b)=> a-b);
+    //구명보트를 최대한 적게 사용하여 모든 사람을 구출
+    //모든 사람을 구출하기 위해 필요한 구명보트 개수
+    //한 번에 두명 탑승
+    let cnt = 0;
+    people = people.sort((a,b) => a-b);
     while(people.length !== 0){
-    if(people[0] + people[people.length - 1] <= limit) { 
-      count++ 
-      people.shift();
-      people.pop();
-    } else {
-      count++
-      people.pop();  
+         if(people.length === 1){
+            cnt ++;
+            break;
+        }
+            
+        if(people[people.length-1] + people[0] <= limit){
+                people.shift();
+                people.pop();
+                cnt++;
+        }
+        else{
+            people.pop();
+            cnt++;
+        }
+        
     }
-    }
-    return count;
+    return cnt;
 }
