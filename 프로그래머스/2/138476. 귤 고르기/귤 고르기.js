9b6aba9c149a582848ed984cd05689ede45d9121
Jo Cheonsan <iii1003@naver.com>
2025-04-18 16:32:18 +0900

    [level 2] Title: 귤 고르기, Time: 96.34 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,20 +1,28 @@
 function solution(k, tangerine) {
-    let size = [];
-    let q = 0;
-    tangerine = tangerine.sort((a,b)=>a-b);
-    for(let i = 0; i < tangerine.length; i++){
-        if(tangerine[i+1] !== tangerine[i]){
-        size.push(i+1 - q);
-        q=i+1;
+    // 과일 크기별 개수를 저장하는 객체 생성
+    const fruitCounts = {};
+    
+    // 각 과일 크기(종류)별 개수 계산
+    for (const size of tangerine) {
+        fruitCounts[size] = (fruitCounts[size] || 0) + 1;
+    }
+    
+    // 개수 기준으로 내림차순 정렬 (많은 개수의 종류부터 선택하면 종류 수를 최소화할 수 있음)
+    const counts = Object.values(fruitCounts).sort((a, b) => b - a);
+    
+    let selectedFruits = 0;
+    let typeCount = 0;
+    
+    // 개수가 많은 종류부터 선택
+    for (const count of counts) {
+        selectedFruits += count;
+        typeCount++;
+        
+        // k개 이상 선택했으면 종료
+        if (selectedFruits >= k) {
+            break;
         }
     }
-    size = size.sort((a,b)=>b-a);
-    let i = 0;
-    let cnt = 0;
-    while(k > 0){
-        k = k-size[i]
-        cnt++;
-        i++;
-    }
-    return cnt;
+    
+    return typeCount;
 }
