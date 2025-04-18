1a12483a5a315054a6ed684c154390cfe684754d
Jo Cheonsan <iii1003@naver.com>
2025-04-18 13:14:25 +0900

    [level 2] Title: 짝지어 제거하기, Time: 30.62 ms, Memory: 51.5 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,34 +1,13 @@
-class Stack{
-    constructor(){
-        this.storage = new Object();
-        this.size = 0;
-    }
-    push(element){
-        this.size++;
-        this.storage[this.size] = element;
-    }
-    pop(){
-        let removed = this.storage[this.size];
-        delete this.storage[this.size];
-        this.size--;
-        return removed;
-    }
-    top(){
-        return this.storage[this.size];
-    }
-}
 function solution(s){
-    const stack = new Stack();
+    const stack = [];
     for(let i = 0; i < s.length; i++){
-        if(stack.top() === s[i]){
+        if(stack.length !== 0 && stack[stack.length - 1] === s[i]){
             stack.pop();
             continue;
         }
-        else{
-            stack.push(s[i]);
-            continue;
-        }}
-return stack.size > 0 ? 0 : 1; 
-}
-   
+        stack.push(s[i]);
+    }
+    return stack.length === 0 ? 1 : 0;
     
+  
+}
