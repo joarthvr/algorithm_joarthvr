b2e220adf2aac8828b75c09de5f4812b31c042e9
Jo Cheonsan <iii1003@naver.com>
2025-04-18 17:38:01 +0900

    [level 2] Title: 2 x n 타일링, Time: 4.22 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -1,14 +1,11 @@
 function solution(n) {
     const MOD = 1000000007;
-    let result = 0;
-    let prev = 1;
-    let curr = 2;
-    if (n === 1) return 1;
-    if (n === 2) return 2;
-    for(let i = 3; i<=n; i++){
-        result = (prev + curr) % MOD;
-        prev = curr;
-        curr = result;
+    const result = [1,2];
+    if(n === 1) return result[1];
+    if(n === 2) return result[2];
+    
+    for(let i = 2; i<n; i++){
+        result[i] = (result[i-2] + result[i-1]) % MOD
     }
-    return result
+    return result[n-1];
 }
