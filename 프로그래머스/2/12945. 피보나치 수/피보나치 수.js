e51337d096ef8caf5fe21ac42780f4b3b3173b17
Jo Cheonsan <iii1003@naver.com>
2025-04-18 13:06:22 +0900

    [level 2] Title: 피보나치 수, Time: 4.40 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,9 +1,12 @@
 function solution(n) {
-    let fib = [];
-    fib[0] = 0;
-    fib[1] = 1;
-    for(let i = 2; i <= n; i++){
-        fib[i] = (fib[i-1] + fib[i-2])%1234567;
+    const memo = [];
+    for(let i = 0; i <=n; i++){
+        if(i <= 1) {
+            memo[i] = i
+        }
+        else{
+            memo[i] = (memo[i-2] + memo[i-1]) % 1234567;
+        }
     }
-    return fib[n]
+    return memo[n];
 }
