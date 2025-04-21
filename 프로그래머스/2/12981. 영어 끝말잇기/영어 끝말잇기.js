284bf69bbd473552197cad4feccb85574bafc2d3
Jo Cheonsan <iii1003@naver.com>
2025-04-21 15:45:43 +0900

    [level 2] Title: 영어 끝말잇기, Time: 4.64 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,19 +1,20 @@
 function solution(n, words) {
-    let cnt = 0;
-    let person = 1;
-    let past = [];
-    for(let i = 0; i < words.length; i++){
-        if(i % n === 0)
-            cnt++;
-        person = (i % n) + 1; // 참가자 번호 계산 수정
-         if(i > 0 && words[i-1][words[i-1].length-1] !== words[i][0]){
-            return [person, cnt];
+    let turn = 1;
+    const len = words.length;
+    const set = new Set();
+    
+    for(let i = 0; i < len; i++){
+        if(i < len - 1){
+            console.log(words[i][words[i].length-1], words[i+1][0], i)
+            if(set.has(words[i])) return [i%n+1, Math.floor(i / n) + 1];
+            if(words[i][words[i].length-1] !== words[i+1][0]) return [(i+1)%n+1, Math.floor((i+1) / n) + 1];    
+            set.add(words[i]);    
         }
-        if(past.includes(words[i])){
-            return [person, cnt];
+        else{
+            if(set.has(words[i])) return [i%n+1, Math.floor(i / n) + 1];
+            if(words[i][words[i][0]] !== words[i-1][words.length-1]) return [(i+1)%n+1, Math.floor((i+1) / n) + 1];
+        }
 
-        }
-        past.push(words[i]);
     }
-     return[0,0]
+    return [0,0];
 }
