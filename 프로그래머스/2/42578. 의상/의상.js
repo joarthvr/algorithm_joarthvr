7cd6519537b7eb635983fe7c321dd0fd97f286fc
Jo Cheonsan <iii1003@naver.com>
2025-04-23 15:48:04 +0900

    [level 2] Title: 의상, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,20 +1,22 @@
 function solution(clothes) {
-    let arr = [];
+    const clothesMap = {};
+    
+    // 카테고리별 의상 개수 계산
     for(let i = 0; i < clothes.length; i++){
-        arr.push(clothes[i][1]);
+        const category = clothes[i][1];
+        if(clothesMap[category]){
+            clothesMap[category]++;
+        } else {
+            clothesMap[category] = 1;
+        }
     }
-    // Step 1: 각 숫자의 빈도를 계산합니다.
-    const frequency = arr.reduce((accu, curr) => {
-        accu[curr] = (accu[curr] || 0) + 1;
-        return accu;
-    }, {});
-    // Step 2: 빈도가 1보다 큰 숫자의 등장 횟수를 배열로 저장합니다.
-    const repeatedCounts = Object.values(frequency)
-        .filter(count => count > 0);
-    const ans = repeatedCounts.reduce((a,e)=>{
-        return a*(e+1);
-    },1);
-    return ans -1;
+    
+    // 모든 조합 계산: (각 카테고리 아이템 수 + 1)을 모두 곱한 후 1 빼기
+    const values = Object.values(clothesMap);
+    let answer = 1;
+    for(let i = 0; i < values.length; i++){
+        answer *= (values[i] + 1); // +1은 해당 카테고리에서 아무것도 선택하지 않는 경우
+    }
+    
+    return answer - 1; // -1은 모든 카테고리에서 아무것도 선택하지 않는 경우(최소 1개는 입어야 함)
 }
-
-
