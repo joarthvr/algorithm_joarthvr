90f0548f4e1a47fc0ea6e4686beb739e1f4bf29a
Jo Cheonsan <iii1003@naver.com>
2025-04-24 21:27:30 +0900

    [level 2] Title: 롤케이크 자르기, Time: 208.62 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -6,29 +6,18 @@ function solution(topping) {
     const leftMap = new Map();
     const rightMap = new Map();
     
-    // 처음에 모든 토핑을 오른쪽에 넣기
-    for (let t of topping) {
+    for(const t of topping){
         rightMap.set(t, (rightMap.get(t) || 0) + 1);
     }
     
-    // 왼쪽으로 하나씩 옮기면서 확인
-    for (let i = 0; i < len - 1; i++) {
-        const t = topping[i];
+    for(let i = 0; i < len - 1; i++){
+        const top = topping.pop();
+        leftMap.set(top, (leftMap.get(top) || 0) + 1);
         
-        // 왼쪽에 추가
-        leftMap.set(t, (leftMap.get(t) || 0) + 1);
+        rightMap.set(top, rightMap.get(top) -1);
+        if(rightMap.get(top) === 0) rightMap.delete(top);
         
-        // 오른쪽에서 제거
-        rightMap.set(t, rightMap.get(t) - 1);
-        if (rightMap.get(t) === 0) {
-            rightMap.delete(t);
-        }
-        
-        // 종류 수가 같으면 카운트 증가
-        if (leftMap.size === rightMap.size) {
-            count++;
-        }
+        if(leftMap.size === rightMap.size) count++;
     }
-    
     return count;
 }
