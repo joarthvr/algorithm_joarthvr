d638ea90c13b1e26b2c0a86ea5b273964aaffabd
Jo Cheonsan <iii1003@naver.com>
2025-04-24 14:38:39 +0900

    [level 2] Title: 튜플, Time: 70.94 ms, Memory: 45.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,25 +1,13 @@
 function solution(s) {
-    // 1. 문자열 파싱: 외부 중괄호({})를 제거하고 각 집합을 추출
-    s = s.substring(2, s.length - 2);
-    const sets = s.split('},{').map(set => 
-        set.split(',').map(Number)
-    );
-    
-    // 2. 집합의 크기에 따라 정렬
-    sets.sort((a, b) => a.length - b.length);
-    
-    // 3. 각 집합을 순회하면서 튜플 구성
+    s = s.substring(2, s.length-2);
+    s = s.split('},{').map((e) => e.split(',')).sort((a,b) => a.length - b.length);
+
     const result = [];
-    const seen = new Set();
-    
-    for (const set of sets) {
-        for (const num of set) {
-            if (!seen.has(num)) {
-                result.push(num);
-                seen.add(num);
-            }
+    for(const element of s){
+        for(let i = 0; i < element.length; i++){
+            const candidate = Number(element[i]);
+            if(!result.includes(candidate)) result.push(candidate);
         }
     }
-    
     return result;
 }
