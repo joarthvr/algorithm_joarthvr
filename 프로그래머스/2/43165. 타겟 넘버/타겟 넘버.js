a7a7ccf2953d0f16f7d304bf52d502bf7bc808e5
Jo Cheonsan <iii1003@naver.com>
2025-04-26 20:58:02 +0900

    [level 2] Title: 타겟 넘버, Time: 36.53 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,22 +1,20 @@
 function solution(numbers, target) {
     let answer = 0;
+    const stack = [{index: 0, sum: 0}];
     
-    function dfs(index, sum) {
-        // 모든 숫자를 다 사용했을 때
+    while (stack.length > 0) {
+        const {index, sum} = stack.pop();
+        
         if (index === numbers.length) {
-            if (sum === target) {
-                answer++;
-            }
-            return;
+            if (sum === target) answer++;
+            continue;
         }
         
-        // 현재 숫자를 더하는 경우
-        dfs(index + 1, sum + numbers[index]);
-        
-        // 현재 숫자를 빼는 경우
-        dfs(index + 1, sum - numbers[index]);
+        // 빼기 경우
+        stack.push({index: index + 1, sum: sum - numbers[index]});
+        // 더하기 경우
+        stack.push({index: index + 1, sum: sum + numbers[index]});
     }
     
-    dfs(0, 0);
     return answer;
 }
