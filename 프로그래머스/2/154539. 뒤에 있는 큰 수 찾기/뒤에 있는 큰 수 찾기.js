70bb0fc9d7b3c9b56255775c0753db87243bb85a
Jo Cheonsan <iii1003@naver.com>
2025-05-01 19:24:58 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 346.66 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -1,12 +1,15 @@
 function solution(numbers) {
-    const result = new Array(numbers.length).fill(-1);
-    const stack = []; // [index, value]
+     const result = Array(numbers.length).fill(-1);
+    const stack = [];
     
-    for(let i = 0; i < numbers.length; i++) {
-        // 스택이 비어있지 않고, 현재 숫자가 스택의 top에 있는 숫자보다 크다면
-        while(stack.length > 0 && numbers[i] > numbers[stack[stack.length-1]]) {
+    for (let i = 0; i < numbers.length; i++) {
+        // 스택이 비어있지 않고, 스택의 top 인덱스의 값이 현재 값보다 작으면
+        while (stack.length > 0 && numbers[stack[stack.length - 1]] < numbers[i]) {
+            // 해당 인덱스의 결과값을 현재 값으로 업데이트
             result[stack.pop()] = numbers[i];
         }
+        
+        // 현재 인덱스를 스택에 추가
         stack.push(i);
     }
     
