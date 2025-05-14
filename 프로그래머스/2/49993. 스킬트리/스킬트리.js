a0f89417f7ca5f528455834d2b30d65503f59a36
Jo Cheonsan <iii1003@naver.com>
2025-05-14 16:24:06 +0900

    [level 2] Title: 스킬트리, Time: 0.27 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -1,19 +1,28 @@
 function solution(skill, skill_trees) {
-    let result = 0;
+    skill = skill.split("");
+    const filteredSkill = [];
+    const arr = [""];  // 빈 문자열 추가!
+    let strSum = ''
     
-    for(const tree of skill_trees) {
-        const queue = [...skill];
-        let isValid = true;
-        for(const s of tree) {
-            if(skill.includes(s)) {
-                if(s !== queue[0]) {
-                    isValid = false;
-                    break;
-                }
-                queue.shift();
-            }
+    for(let i = 0; i < skill.length; i++){
+        strSum += skill[i];
+        arr.push(strSum);
+    }
+    
+    for(let i = 0; i < skill_trees.length; i++){
+        let tmp = '';
+        for(let j = 0; j < skill_trees[i].length; j++){
+            // skill에 포함된 문자인지 확인
+            if(skill.includes(skill_trees[i][j])){
+                tmp += skill_trees[i][j];
+            }    
         }
-        if(isValid) result++;
+        filteredSkill.push(tmp)
+    }
+    
+    let result = 0;
+    for(let i = 0; i < filteredSkill.length; i++){
+        if(arr.includes(filteredSkill[i])) result++;
     }
     return result;
 }
