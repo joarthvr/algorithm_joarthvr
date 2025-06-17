ce99864b14329c9eb433626abdc01cd5edc373a9
Jo Cheonsan <iii1003@naver.com>
2025-06-17 14:32:55 +0900

    [level 2] Title: 광물 캐기, Time: 9.11 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
@@ -1,57 +1,50 @@
 function solution(picks, minerals) {
-    const [dia, iron, stone] = picks;
-    const totalPicks = dia + iron + stone;
+    let [dia, iron, stone] = picks;
+    const totalTools = (dia + iron + stone)*5;
+    const actualMinerals = minerals.slice(0, totalTools);
     
-    // 실제로 캘 수 있는 광물만 고려
-    const actualMinerals = minerals.slice(0, totalPicks * 5);
-    
-    // 5개씩 그룹으로 나누고 각 그룹의 비용 계산
     const groups = [];
-    for (let i = 0; i < actualMinerals.length; i += 5) {
-        const group = actualMinerals.slice(i, i + 5);
-        
-        // 돌 곡갱이로 캤을 때의 피로도 (정렬 기준)
-        let stoneCost = 0;
-        for (const mineral of group) {
-            if (mineral === "diamond") stoneCost += 25;
-            else if (mineral === "iron") stoneCost += 5;
-            else stoneCost += 1;
+    for(let i = 0; i < actualMinerals.length; i += 5){
+        const slicedMinerals = actualMinerals.slice(i,i + 5);
+        let cost = 0;
+        for(let j = 0; j < slicedMinerals.length; j++){
+            if(slicedMinerals[j] === 'diamond') cost += 25;
+            if(slicedMinerals[j] === 'iron') cost += 5;
+            if(slicedMinerals[j] === 'stone') cost += 1;
         }
-        
-        groups.push({ minerals: group, cost: stoneCost });
+        groups.push({slicedMinerals, cost})
     }
-    
-    // 비용이 높은 그룹부터 정렬
-    groups.sort((a, b) => b.cost - a.cost);
-    
-    // 좋은 곡갱이부터 사용
-    let totalFatigue = 0;
-    let diaCount = dia, ironCount = iron, stoneCount = stone;
-    
-    for (const group of groups) {
-        let pickType;
-        if (diaCount > 0) {
-            pickType = 0; // 다이아몬드 곡갱이
-            diaCount--;
-        } else if (ironCount > 0) {
-            pickType = 1; // 철 곡갱이
-            ironCount--;
-        } else {
-            pickType = 2; // 돌 곡갱이
-            stoneCount--;
-        }
-        
-        // 해당 곡갱이로 그룹의 광물들 채굴
-        for (const mineral of group.minerals) {
-            if (mineral === "diamond") {
-                totalFatigue += [1, 5, 25][pickType];
-            } else if (mineral === "iron") {
-                totalFatigue += [1, 1, 5][pickType];
-            } else { // stone
-                totalFatigue += [1, 1, 1][pickType];
+    groups.sort((a,b) => b.cost - a.cost);
+    console.log(groups)
+    let result = 0;
+    for(const group of groups){
+        for(let i = 0; i < group.slicedMinerals.length; i++){
+            if(dia > 0){
+              if(group.slicedMinerals[i] === 'diamond') result += 1;
+                if(group.slicedMinerals[i] === 'iron') result += 1;
+                if(group.slicedMinerals[i] === 'stone') result += 1;
             }
+            else if(iron > 0){
+                if(group.slicedMinerals[i] === 'diamond') result += 5;
+                if(group.slicedMinerals[i] === 'iron') result += 1;
+                if(group.slicedMinerals[i] === 'stone') result += 1;
+            }
+            else if(stone > 0){
+                if(group.slicedMinerals[i] === 'diamond') result += 25;
+                if(group.slicedMinerals[i] === 'iron') result += 5;
+                if(group.slicedMinerals[i] === 'stone') result += 1;
+            }
+            
+            console.log(dia,iron,stone, result)
+        }
+        if(dia > 0) {
+            dia--;
+        }
+        else if(iron > 0){
+            iron--;
+        }else if(stone > 0){
+            stone--;
         }
     }
-    
-    return totalFatigue;
+    return result;
 }
