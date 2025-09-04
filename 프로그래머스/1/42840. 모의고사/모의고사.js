d2c798cf6a23bff97b612ab3ff8fa9d887292b1c
Jo Cheonsan <iii1003@naver.com>
2025-09-04 16:15:54 +0900

    [level 1] Title: 모의고사, Time: 6.35 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,28 +1,22 @@
 function solution(answers) {
-    let a = [1, 2, 3, 4, 5];
-    let b = [2, 1, 2, 3, 2, 4, 2, 5];
-    let c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    const scores = [0,0,0];
     
-    // 맞춘 개수를 저장할 객체
-    let cnt = { 1: 0, 2: 0, 3: 0 };
-
-    // 각 답안 패턴과 정답을 비교하여 맞춘 개수 세기
-    for (let i = 0; i < answers.length; i++) {
-        if (answers[i] === a[i % a.length]) cnt[1]++;
-        if (answers[i] === b[i % b.length]) cnt[2]++;
-        if (answers[i] === c[i % c.length]) cnt[3]++;
+    const p1 = [1, 2, 3, 4, 5];
+    const p2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    const p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    
+    for(let i = 0; i < answers.length; i++){
+        if(answers[i] === p1[i % p1.length]) scores[0]++;
+        if(answers[i] === p2[i % p2.length]) scores[1]++;
+        if(answers[i] === p3[i % p3.length]) scores[2]++;
     }
-
-    // 최대 맞춘 개수 찾기
-    let maxScore = Math.max(cnt[1], cnt[2], cnt[3]);
-
-    // 최대 맞춘 개수를 가진 사람 찾기
-    let result = [];
-    for (let key in cnt) {
-        if (cnt[key] === maxScore) {
-            result.push(Number(key));
+    
+    const max = Math.max(...scores);
+    const result = [];
+    for(let i = 0; i < scores.length; i++){
+        if(scores[i] === max){
+            result.push(i+1);
         }
-    }
-
+    }   
     return result;
 }
