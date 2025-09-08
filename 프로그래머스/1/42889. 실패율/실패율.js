bdbe042601c595e470f3e5f12d140ef905510756
Jo Cheonsan <iii1003@naver.com>
2025-09-08 17:53:09 +0900

    [level 1] Title: 실패율, Time: 5233.91 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,11 +1,11 @@
-  function solution(N, stages) {
-    let result = [];
-    for(let i=1; i<=N; i++){
-        let reach = stages.filter((x) => x >= i).length;
-        let curr = stages.filter((x) => x === i).length;
-        result.push([i, curr/reach]);
-       
+function solution(N, stages) {
+    const failureRateArr = new Array(N).fill(0);
+    
+    
+    for(let i = 1; i <= N; i++){
+        failureRateArr[i-1] = stages.filter((e) => e === i).length / stages.filter((e) => e >= i).length;    
     }
-    result.sort((a,b) => b[1] - a[1]);
-    return result.map((x) => x[0]);
+    const indices = Array.from(failureRateArr.keys());
+    const sortedIndices = indices.sort((a, b) => failureRateArr[b] - failureRateArr[a]).map((e) => e + 1);
+    return sortedIndices;
 }
