265cbf8c57b76c055f52618cffc375709428deec
조천산 (Jo Cheonsan) <iii1003@naver.com>
2025-12-26 23:59:48 +0900

    [level 2] Title: 숫자의 표현, Time: 0.36 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,7 +1,9 @@
-function solution(n) {
-  let answer = 0;
-  for(let i = 0; i <= n; i++) {
-  	if(n%i === 0 && i%2 === 1) answer++;
-  }
-  return answer;
+function solution(num) {
+    var answer = 0;
+    for (var i = 1; i <= num; i++) {
+        if ((num % i == 0) && (i % 2 == 1)) {
+            answer++;
+        }
+    }
+    return answer;
 }
