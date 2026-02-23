3dba558b8958238de2396058ceb08d8c996058d7
조천산 (Jo Cheonsan) <iii1003@naver.com>
2026-02-24 01:18:47 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.56 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,9 +1,11 @@
 function solution(numbers) {
-    const tmp = [];
-    for(let i = 0; i < numbers.length; i++){
-        for(let j = i + 1 ; j < numbers.length; j++ ){
-            tmp.push(numbers[i] + numbers[j]);
+    const result = [];
+    const len = numbers.length;
+    for(let i = 0; i < len; i++){
+        for(let j = i + 1; j < len; j++){
+            result.push(numbers[i] + numbers[j]);
         }
     }
-    return ([...new Set(tmp)].sort((a,b) => a-b));
+    return [...new Set(result)].sort((a,b) => a-b);
+    
 }
