28fb3c937ea6480b5ba805d7298d59a78ae134d8
조천산 (Jo Cheonsan) <iii1003@naver.com>
2026-02-24 01:55:44 +0900

    [level 1] Title: 모의고사, Time: 29.99 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,22 +1,22 @@
 function solution(answers) {
-    const scores = [0,0,0];
+    const patterns = [[1,2,3,4,5], [2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]];
+    const result = new Array(3).fill(0);
     
-    const p1 = [1, 2, 3, 4, 5];
-    const p2 = [2, 1, 2, 3, 2, 4, 2, 5];
-    const p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-    
-    for(let i = 0; i < answers.length; i++){
-        if(answers[i] === p1[i % p1.length]) scores[0]++;
-        if(answers[i] === p2[i % p2.length]) scores[1]++;
-        if(answers[i] === p3[i % p3.length]) scores[2]++;
+    for(let i = 0; i < patterns.length; i++){
+        for(let j = 0; j < answers.length; j++){
+            if(answers[j] === patterns[i][j % patterns[i].length]){
+                result[i]++;
+            }
+        }
+    }
+    const max = Math.max(...result);
+    const result1 = [];
+    for(let i = 0; i < result.length; i++){
+        if(result[i] === max){
+            result1.push(i+1);
+        }
+        
     }
     
-    const max = Math.max(...scores);
-    const result = [];
-    for(let i = 0; i < scores.length; i++){
-        if(scores[i] === max){
-            result.push(i+1);
-        }
-    }   
-    return result;
+    return result1;
 }
