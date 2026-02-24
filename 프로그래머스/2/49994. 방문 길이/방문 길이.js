ce897bd073abdfcc3e7fb71781e10a0ece353755
조천산 (Jo Cheonsan) <iii1003@naver.com>
2026-02-24 18:14:48 +0900

    [level 2] Title: 방문 길이, Time: 0.66 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,40 +1,36 @@
 function solution(dirs) {
-    // 현재 위치
-    let x = 0;
-    let y = 0;
-    
-    // 방문한 경로를 저장할 Set (양방향 경로 처리를 위해)
-    const visitedPaths = new Set();
-    
-    for (let i = 0; i < dirs.length; i++) {
-        // 이전 위치 저장
-        let prevX = x;
-        let prevY = y;
-        
-        // 방향에 따라 이동
-        if (dirs[i] === 'U' && y < 5) {
-            y += 1;
-        } else if (dirs[i] === 'D' && y > -5) {
-            y -= 1;
-        } else if (dirs[i] === 'R' && x < 5) {
-            x += 1;
-        } else if (dirs[i] === 'L' && x > -5) {
-            x -= 1;
-        } else {
-            // 범위를 벗어나는 경우 무시 (위치 변화 없음)
-            continue;
-        }
-        
-        // 경로 표현 (양방향을 고려하여 작은 좌표를 먼저 표기)
-        const path = prevX < x || (prevX === x && prevY < y) 
-            ? `${prevX},${prevY}-${x},${y}` 
-            : `${x},${y}-${prevX},${prevY}`;
-        
-        // 방문한 경로 추가
-        visitedPaths.add(path);
-    }
-    
-    // 유니크한 경로의 수 반환
-    return visitedPaths.size;
-}
+  const set = new Set();
+  let [x, y] = [0, 0];
 
+  const dirMap = new Map([
+    ['U', { x: 0,  y: 1 }],
+    ['D', { x: 0,  y: -1 }],
+    ['R', { x: 1,  y: 0 }],
+    ['L', { x: -1, y: 0 }],
+  ]);
+
+  for (const dir of dirs) {
+    const move = dirMap.get(dir);
+    const prevX = x;
+    const prevY = y;
+
+    x += move.x;
+    y += move.y;
+
+    if (x > 5) x = 5;
+    if (x < -5) x = -5;
+    if (y > 5) y = 5;
+    if (y < -5) y = -5;
+
+    if (prevX === x && prevY === y) continue;
+
+    const key =
+      (prevX < x || (prevX === x && prevY < y))
+        ? `${prevX},${prevY},${x},${y}`
+        : `${x},${y},${prevX},${prevY}`;
+
+    set.add(key);
+  }
+
+  return set.size;
+}
