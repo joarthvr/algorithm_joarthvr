7adbe2dc7e34b397322cc2c1cba20c139aeeeb93
조천산 (Jo Cheonsan) <iii1003@naver.com>
2026-02-26 23:45:40 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.41 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,34 +1,30 @@
 function solution(board, moves) {
-    let board1 = [];
-    
-    // 보드 트랜스포즈
-    for (let i = 0; i < board.length; i++) {
-        let tmpArr = [];
-        for (let j = 0; j < board.length; j++) {
-            if (board[j][i] === 0)
-                continue;
-            tmpArr.push(board[j][i]);
+    const n = board.length;
+    const stack = [];
+    const map = new Map();
+    let result = 0;
+    for(let i = 0; i < n; i++){
+        const tmp = [];
+        for(let j = 0; j < n; j++){
+            if(board[j][i] !== 0) tmp.push(board[j][i]);
         }
-        board1.push(tmpArr);
+        map.set(i+1,tmp.reverse())
+    }
+    for(let i = 0; i < moves.length; i++){
+        const target = moves[i];
+        if(map.get(target).length > 0){
+          const picked = map.get(target).pop();  
+            if(stack.length !== 0 && stack[stack.length - 1] === picked) {
+                stack.pop();
+                result += 2;
+                continue;
+            }
+            stack.push(picked)
+        }
+        else {
+            continue;
+        }
     }
     
-    let basket = [];
-    let result = 0;
-    
-    // moves를 처리하여 인형을 바구니에 추가
-    moves.forEach((e, index) => {
-        if (board1[e-1] && board1[e-1].length > 0) {
-            let item = board1[e-1][0]; // 첫 번째 아이템을 가져옵니다.
-            board1[e-1].shift(); // 첫 번째 아이템을 배열에서 제거합니다.
-            
-            if (basket.length > 0 && basket[basket.length - 1] === item) {
-                basket.pop(); // 바구니의 마지막 아이템과 동일하면 제거
-                result += 2; // 제거된 인형 수를 2개 증가
-            } else {
-                basket.push(item); // 그렇지 않으면 바구니에 추가
-            }
-        }
-    });
-
     return result;
 }
