5ec3ee1af285a7da383d663f19b278c8aab0bd78
조천산 (Jo Cheonsan) <iii1003@naver.com>
2026-03-03 16:13:38 +0900

    [level 2] Title: 할인 행사, Time: 85.29 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,32 +1,22 @@
 function solution(want, number, discount) {
-    let result = 0;
-    
-    // 원하는 상품과 수량을 맵으로 구성
-    const wantMap = {};
-    for(let i = 0; i < want.length; i++) {
-        wantMap[want[i]] = number[i];
+    let ans = 0;
+    const obj = {};
+    const n = want.length;
+    for(let i = 0; i < n; i++){
+        obj[want[i]] = number[i];
     }
     
-    // 10일 윈도우를 이동하며 체크
-    for(let i = 0; i <= discount.length - 10; i++) {
-        // 현재 10일간의 할인 상품 카운트
-        const countMap = {};
-        for(let j = 0; j < 10; j++) {
-            const item = discount[i + j];
-            countMap[item] = (countMap[item] || 0) + 1;
+    for(let i = 0; i <= discount.length - 10; i++){
+        const copy = {...obj};
+        let idx = i;
+        while(i + 9 >= idx){
+        if(copy[discount[idx]] > 0){
+            copy[discount[idx]] = copy[discount[idx]] - 1;
         }
-        
-        // 원하는 모든 상품이 충족되는지 확인
-        let allSatisfied = true;
-        for(let w of want) {
-            if(countMap[w] !== wantMap[w]) {
-                allSatisfied = false;
-                break;
-            }
+            idx++;
         }
-        
-        if(allSatisfied) result++;
+        const result = [...new Set(Object.values(copy))];
+        if(result.length === 1 && result[0] === 0) ans++;
     }
-    
-    return result;
+    return ans
 }
