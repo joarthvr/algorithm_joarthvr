56757ae343d1b75b23e947e49c32468fbd2e9ad7
조천산 (Jo Cheonsan) <iii1003@naver.com>
2026-03-06 17:37:23 +0900

    [level 2] Title: 예상 대진표, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,11 +1,11 @@
-function solution(n,a,b)
-{
-    let cnt = 1;
-    while(Math.ceil(a/2)!==Math.ceil(b/2)){
-        a = Math.ceil(a/2);
-        b = Math.ceil(b/2);
+function solution(n, a, b) {
+    let cnt = 0;
+
+    while (a !== b) {
+        a = Math.ceil(a / 2);
+        b = Math.ceil(b / 2);
         cnt++;
     }
-    
+
     return cnt;
 }
