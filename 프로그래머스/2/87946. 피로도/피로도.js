8610fa26a1927e8267ab7c2289c3bf6735ce709f
조천산 (Jo Cheonsan) <iii1003@naver.com>
2026-03-13 13:41:12 +0900

    [level 2] Title: 피로도, Time: 31.62 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,18 +1,21 @@
 function solution(k, dungeons) {
-    const visited = new Array(dungeons.length).fill(false);
     let maxCount = 0;
-    const dfs = (fatigue, count) => {
+    const visited = new Array(dungeons.length).fill(false);
+
+    function dfs(fatigue, count) {
         maxCount = Math.max(maxCount, count);
-        
-        for(let i = 0; i < dungeons.length; i++){
-            const [minFatigue, costFatigue] = dungeons[i];
-            if(!visited[i] && fatigue >= minFatigue){
+
+        for (let i = 0; i < dungeons.length; i++) {
+            const [minNeed, cost] = dungeons[i];
+
+            if (!visited[i] && fatigue >= minNeed) {
                 visited[i] = true;
-                dfs(fatigue - costFatigue, count + 1);
+                dfs(fatigue - cost, count + 1);
                 visited[i] = false;
             }
         }
     }
+
     dfs(k, 0);
     return maxCount;
 }
