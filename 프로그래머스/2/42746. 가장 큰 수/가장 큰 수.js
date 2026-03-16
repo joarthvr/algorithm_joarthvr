256eff8dd8e3cac9dfe62cec667de59e26c4b754
조천산 (Jo Cheonsan) <iii1003@naver.com>
2026-03-16 15:53:57 +0900

    [level 2] Title: 가장 큰 수, Time: 176.74 ms, Memory: 44.8 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,17 +1,10 @@
 function solution(numbers) {
-    // 숫자를 문자열로 변환
-    const strNumbers = numbers.map(num => num.toString());
-    
-    // 두 수를 이어붙였을 때 더 큰 수가 되는 순서로 정렬
-    strNumbers.sort((a, b) => {
-        return (b + a) - (a + b);
-    });
-    
-    // 모든 숫자가 0인 경우 (예: [0, 0, 0]) "0"을 반환
-    if (strNumbers[0] === '0') {
-        return '0';
+    function compare(a,b){
+        const n1 = Number(`${a}${b}`);
+        const n2 = Number(`${b}${a}`);
+        return n1 > n2 ? -1 : 1;
     }
     
-    // 정렬된 숫자들을 이어붙여 반환
-    return strNumbers.join('');
+    numbers = numbers.sort(compare).join("");
+    return Number(numbers) === 0 ? "0" : numbers;
 }
