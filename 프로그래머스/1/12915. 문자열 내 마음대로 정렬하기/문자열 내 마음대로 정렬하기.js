f70fd94564e86c18e7544e1da1560bc1c9154fe9
조천산 (Jo Cheonsan) <iii1003@naver.com>
2026-03-16 15:18:06 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,7 +1,5 @@
 function solution(strings, n) {
-    
-    return strings.sort((s1, s2) => s1[n] === s2[n] ? 
-                        s1.localeCompare(s2) : s1[n].localeCompare(s2[n]));
+    strings.sort();
+    strings.sort((a,b) => a[n].charCodeAt(0) - b[n].charCodeAt(0));
+    return strings;
 }
-
-
