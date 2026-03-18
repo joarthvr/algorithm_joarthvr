fada8445ddbdcdf0b14da85e8ae1ab985c13cdbe
조천산 (Jo Cheonsan) <iii1003@naver.com>
2026-03-18 13:47:43 +0900

    [level 2] Title: 롤케이크 자르기, Time: 117.40 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -1,23 +1,20 @@
 function solution(topping) {
-    let count = 0;
-    const len = topping.length;
+    let ans = 0;
     
-    // 왼쪽과 오른쪽의 토핑 종류 개수를 미리 계산
-    const leftMap = new Map();
-    const rightMap = new Map();
+    const ls = new Set();
+    const rm = new Map();
     
     for(const t of topping){
-        rightMap.set(t, (rightMap.get(t) || 0) + 1);
+        rm.set(t, (rm.get(t) || 0) + 1);
     }
-    
-    for(let i = 0; i < len - 1; i++){
-        const top = topping.pop();
-        leftMap.set(top, (leftMap.get(top) || 0) + 1);
+    for(let i = 0; i < topping.length - 1; i++){
+        const t = topping[i];
+        ls.add(t);
+        rm.set(t, rm.get(t) - 1);
+        if(rm.get(t) === 0) rm.delete(t);
         
-        rightMap.set(top, rightMap.get(top) -1);
-        if(rightMap.get(top) === 0) rightMap.delete(top);
-        
-        if(leftMap.size === rightMap.size) count++;
+        if(ls.size === rm.size) ans++;
     }
-    return count;
+    return ans;
 }
+
