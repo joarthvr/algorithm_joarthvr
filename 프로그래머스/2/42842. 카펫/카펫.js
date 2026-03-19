af5bba3e50bb397cf65faa84073c18ade94b4c03
조천산 (Jo Cheonsan) <iii1003@naver.com>
2026-03-19 12:09:16 +0900

    [level 2] Title: 카펫, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,16 +1,14 @@
 function solution(brown, yellow) {
-    const total = brown + yellow;
-    
-    // 가능한 모든 세로 길이에 대해 검사
-    for (let height = 3; height <= Math.sqrt(total); height++) {
-        // 전체 면적이 height로 나누어 떨어지는지 확인
-        if (total % height === 0) {
-            const width = total / height;
-            
-            // 갈색 격자 수 조건 확인
-            if (2 * width + 2 * height - 4 === brown) {
-                return [width, height]; // [가로, 세로] 반환
-            }
-        }
+    const arr = [];
+    for(let i = 1; i <= Math.sqrt(yellow); i++){
+        if(yellow % i === 0) arr.push([i, yellow / i])
+    }
+    for(const [ls, bs] of arr){
+        if(brown - bs * 2 < (ls + 2) * 2) continue;
+        
+        return [bs + 2, ls + 2]
+        
     }
 }
+
+
