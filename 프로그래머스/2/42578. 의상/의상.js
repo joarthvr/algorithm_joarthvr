fbb9629895e3d974d2ebdd2da2cb3207e50b8ca3
조천산 (Jo Cheonsan) <iii1003@naver.com>
2026-04-03 22:41:11 +0900

    [level 2] Title: 의상, Time: 0.33 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,22 +1,10 @@
 function solution(clothes) {
-    const clothesMap = {};
+    const obj = {};
+    for(const [clo,cate] of clothes){
+        obj[cate] = (obj[cate] || 1) +1
+    } 
+    const num = Object.values(obj).reduce((acc,cur)=> acc * cur,1);
+    return num - 1;
     
-    // 카테고리별 의상 개수 계산
-    for(let i = 0; i < clothes.length; i++){
-        const category = clothes[i][1];
-        if(clothesMap[category]){
-            clothesMap[category]++;
-        } else {
-            clothesMap[category] = 1;
-        }
-    }
     
-    // 모든 조합 계산: (각 카테고리 아이템 수 + 1)을 모두 곱한 후 1 빼기
-    const values = Object.values(clothesMap);
-    let answer = 1;
-    for(let i = 0; i < values.length; i++){
-        answer *= (values[i] + 1); // +1은 해당 카테고리에서 아무것도 선택하지 않는 경우
-    }
-    
-    return answer - 1; // -1은 모든 카테고리에서 아무것도 선택하지 않는 경우(최소 1개는 입어야 함)
 }
