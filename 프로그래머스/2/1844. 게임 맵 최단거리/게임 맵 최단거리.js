b170c0b9c93dc06a64cdbfe588c4f3c1f378f0ea
조천산 (Jo Cheonsan) <iii1003@naver.com>
2026-04-03 21:53:23 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 51.20 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,46 +1,23 @@
-class Queue {
-    constructor() {
-        this.q = [];
-        this.head = 0;
-        this.tail = 0;
-    }
-    enqueue(v) {this.q[this.tail++] = v;}
-    dequeue() {return this.head === this.tail ? undefined : this.q[this.head++];}
-    isEmpty() {return this.head === this.tail;}
-}
-
 function solution(maps) {
     const n = maps.length;
     const m = maps[0].length;
-
-    const visited = Array.from({ length: n }, () => Array(m).fill(false));
-    const q = new Queue();
-
-    q.enqueue([0, 0, 1]);
-    visited[0][0] = true;
-
-    const directions = [[-1,0],[1,0],[0,-1],[0,1]];
-
-    while (!q.isEmpty()) {
-        const [x, y, dist] = q.dequeue();
-
-        if (x === n - 1 && y === m - 1) return dist;
-
-        for (const [dx, dy] of directions) {
-            const nx = x + dx;
-            const ny = y + dy;
-
-            if (
-                nx >= 0 && nx < n &&
-                ny >= 0 && ny < m &&
-                maps[nx][ny] === 1 &&
-                !visited[nx][ny]
-            ) {
-                visited[nx][ny] = true;
-                q.enqueue([nx, ny, dist + 1]);
+    const visited = Array.from({length:n}, ()=>Array(m).fill(false));
+    const q = [[0,0,1]];
+    let idx = 0;
+    const d = [[0,1],[0,-1],[1,0],[-1,0]];
+    while(idx < q.length){
+        const [cR,cC,dist] = q[idx++];
+        if(cR === n - 1 && cC === m - 1) return dist;
+        visited[cR][cC] = true;
+        for(const [dR,dC] of d){
+            const nR = cR + dR;
+            const nC = cC + dC;
+            if(nR >= n || nR < 0 || nC >= m || nC < 0 || maps[nR][nC] === 0) continue; 
+            if(!visited[nR][nC]){
+                visited[nR][nC] = true;
+                q.push([nR,nC, dist + 1]);
             }
         }
     }
-    console.log(1)
     return -1;
 }
