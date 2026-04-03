a00ff44396d5918763999493467ba1b4db2486cd
조천산 (Jo Cheonsan) <iii1003@naver.com>
2026-04-04 00:57:54 +0900

    [level 2] Title: 소수 찾기, Time: 13.23 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,34 +1,35 @@
-function isPrime(n) {
-	if (n < 2) return false;
-	for (let i = 2; i <= Math.sqrt(n); i++) {
-		if (n % i === 0) return false;
-	}
-	return true;
-}
-
-function dfs(set, arr, fixed) {
-	if (arr.length >= 1) {
-		for (let i = 0; i < arr.length; i++) {
-			let newFixed = fixed + arr[i];
-			let copyArr = [...arr];
-			copyArr.splice(i, 1);
-
-			if (isPrime(parseInt(newFixed))) {
-				set.add(parseInt(newFixed));
-			}
-
-			dfs(set, copyArr, newFixed);
-		}
-	}
-}
-
 function solution(numbers) {
-	let nums = numbers.split("");
-	let set = new Set();
-
-	dfs(set, nums, '');
-	
-	console.log(set);
-
-	return set.size;
+    const n = numbers.split("");
+    const len = n.length;
+    function isPrime(num){
+        if(num < 2) return false;
+        for (let i = 2; i <= Math.sqrt(num); i++){
+            if(num % i === 0) return false
+        }
+        return true
+    }
+    function dfs(node, visited, arr, basket){
+        if(arr.length > 0) {
+            const num = Number(arr.join(""));
+            if(isPrime(num)) basket.add(num);
+        };
+        visited[node] = true;
+         for(let i = 0 ; i < len; i++){
+            if(!visited[i]) {
+                const tmp = [...arr];
+                tmp.push(n[i]);
+                dfs(i,visited, tmp, basket);
+                visited[i]=false;
+            }
+        }
+    }
+    const set = new Set()
+    for(let i = 0 ; i < len; i++){
+        const visited = new Array(len).fill(false);
+        dfs(i,visited, [n[i]], set);
+    }
+    return set.size;
 }
+
+
+
