ccbdb43cc830ccb1165e159c82c86c2d065df99a
조천산 (Jo Cheonsan) <iii1003@naver.com>
2026-04-05 13:07:38 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 23.13 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,23 +1,24 @@
 function solution(maps) {
-    const n = maps.length, m = maps[0].length;
-    const visited = Array.from({length : n},()=>Array(m).fill(false));
-    const d = [[0,-1],[0,1],[-1,0],[1,0]];
-    const q = [[0,0,1]];
-    let ans = 1;
+    const n = maps.length;
+    const m = maps[0].length;
+    const q = [];
+    const visited = Array.from({length : n},()=>new Array(m).fill(false));
+    q.push([0,0,1])
     visited[0][0] = true;
     let idx = 0;
+    const d = [[1,0], [-1,0], [0,-1], [0,1]];
+    
     while(idx < q.length){
-        let [cR,cC,cnt] = q[idx++];
-        if(cR === n-1 && cC === m-1) return cnt;
+        const [cN,cM,pt] = q[idx++];
+        if(cN === n - 1 && cM === m - 1) return pt;
         for(let i = 0; i < d.length; i++){
-            const [dR, dC] = d[i];
-            const [nR, nC] = [cR + dR, cC + dC];
-            if(nR < 0 || nR >= n || nC < 0 || nC >= m || maps[nR][nC] === 0 || visited[nR][nC]) continue;
-            visited[nR][nC] = true;
-            q.push([nR, nC, cnt + 1]);
-            
+            const [dN,dM] = d[i];
+            const nN = cN + dN;  
+            const nM = cM + dM;
+            if( nM >= m || nM < 0 || nN >= n || nN < 0 || visited[nN][nM] || maps[nN][nM] === 0) continue;
+            q.push([nN,nM,pt+1]);
+            visited[nN][nM] = true;
         }
     }
-    
-    return -1;
+ return -1;   
 }
