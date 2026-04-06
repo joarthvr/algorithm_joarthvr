1c3aa393781fcbfb298cd2276ca086553be493c9
조천산 (Jo Cheonsan) <iii1003@naver.com>
2026-04-06 23:25:09 +0900

    [level 2] Title: 배달, Time: 6.55 ms, Memory: 35.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -1,3 +1,32 @@
+function solution(N, road, K) {
+    const g = Array.from({length : N + 1}, ()=> []);
+    
+    //인접 리스트
+    for(const [a,b,c] of road){
+        g[a].push([b,c]);
+        g[b].push([a,c]);
+    }
+
+    const dist = new Array(N+1).fill(Infinity);
+    dist[1] = 0; 
+    const heap = new Heap((a,b)=>a[0]-b[0]);
+    heap.push([0,1])// [거리, 위치]
+    
+    while(heap.size() > 0){
+        const [cost, cur] = heap.pop();
+        if(cost > dist[cur]) continue;
+        for(const [next,weight] of g[cur]){
+            const newCost = cost + weight;
+            if(newCost < dist[next]){
+                dist[next] = newCost;
+                heap.push([newCost, next]);
+            }
+        }
+    }
+    console.log(11)
+    return dist.filter(d => d <= K).length;
+}
+
 class Heap{
     constructor(compare){
         this.heap = [];
@@ -6,16 +35,15 @@ class Heap{
     push(v){
         this.heap.push(v);
         let idx = this.heap.length - 1;
-        
         while(idx > 0){
             const p = Math.floor((idx - 1) / 2);
-            if(this.compare(this.heap[p], this.heap[idx]) <= 0) break;
+            if(this.compare(this.heap[p], this.heap[idx]) <= 0)break;
             [this.heap[p], this.heap[idx]] = [this.heap[idx], this.heap[p]];
             idx = p;
         }
     }
     pop(){
-        if(this.heap.length === 0) undefined;
+        if(this.heap.length === 0) return undefined;
         if(this.heap.length === 1) return this.heap.pop();
         const top = this.heap[0];
         this.heap[0] = this.heap.pop();
@@ -31,9 +59,11 @@ class Heap{
             if(r < len && this.compare(this.heap[r], this.heap[c]) < 0) c = r;
             
             if(c === idx) break;
-            [this.heap[c], this.heap[idx]] = [this.heap[idx], this.heap[c]];
+            
+            [this.heap[c], this.heap[idx]] = [this.heap[idx], this.heap[c]]; 
             idx = c;
         }
+        
         return top;
         
     }
@@ -43,32 +73,4 @@ class Heap{
     peek(){
         return this.heap[0];
     }
-}
-function solution(N, road, K) {
-    const graph = Array.from({length: N + 1}, () => []);
-    for(const [a,b,w] of road){
-        graph[a].push([b,w]);
-        graph[b].push([a,w]);
-        
-    }
-    
-    const dist = Array(N + 1).fill(Infinity);
-    dist[1] = 0;
-    const heap = new Heap((a, b) => a[0] - b[0]);
-    heap.push([0, 1]);
-    
-    
-    while(heap.size() > 0){
-        const [cost, cur] = heap.pop();
-        if(cost > dist[cur]) continue;
-        for(const [next, weight] of graph[cur]){
-            const newCost = cost + weight;
-            if(newCost < dist[next]){
-                dist[next] = newCost;
-                heap.push([newCost, next]);
-            }
-        }
-    }
-    
-    return dist.filter(d => d <= K).length;
 }
