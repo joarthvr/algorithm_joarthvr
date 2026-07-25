cd9948a0f7ab6566652842eeba0937f83cc1523d
조천산 (Jo Cheonsan) <iii1003@naver.com>
2026-07-25 17:38:32 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.33 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -0,0 +1,20 @@
+import heapq
+
+def solution(jobs):
+    jobs.sort(key=lambda x: x[0])
+    heap = []
+    current_time, i, answer = 0, 0, 0
+    start = -1
+
+    while i < len(jobs) or heap:
+        while i < len(jobs) and jobs[i][0] <= current_time:
+            heapq.heappush(heap, (jobs[i][1], jobs[i][0]))
+            i += 1
+        if heap:
+            duration, request_time = heapq.heappop(heap)
+            current_time += duration
+            answer += current_time - request_time
+        else:   
+            current_time = jobs[i][0]
+    
+    return answer // len(jobs)
