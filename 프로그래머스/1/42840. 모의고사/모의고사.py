6fa00be1471b0792dafd4beb9f45db754627dc55
조천산 (Jo Cheonsan) <iii1003@naver.com>
2026-07-25 23:01:40 +0900

    [level 1] Title: 모의고사, Time: 1.44 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,6 +1,6 @@
 def solution(answers):
     patterns = [[1,2,3,4,5], [2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]];
-    ans, result = [], []
+    ans = []
     max = 0
     for j, p in enumerate(patterns):
         cnt = 0
